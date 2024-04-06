4c06e0f42caf57b3c335a6ed7bebed8378d5cbcc
chamath-shanaka <chamathshanaka19@gmail.com>
2024-04-06 18:58:46 +0530

    comments

--- src/components/DesktopNav.jsx
+++ src/components/DesktopNav.jsx
@@ -1,27 +1,3 @@
-// export default function DesktopNav() {
-//   return (
-//     <nav className="bg-blueishPurple p-5 text-white sticky top-0 flex justify-between items-center">
-//       <span className="">
-//         <img src="../img/AT Digital Logo.svg" alt="logo" />
-//       </span>
-//       <div className="hidden md:flex flex-col md:flex-row items-center">
-//         <ul className="flex flex-col md:flex-row">
-//           <li className="mb-4 md:ml-6 md:mb-0">SERVICES</li>
-//           <li className="mb-4 md:ml-6 md:mb-0">ABOUT US</li>
-//           <li className="mb-4 md:ml-6 md:mb-0">CONTACT US</li>
-//           <li className="mb-4 md:ml-6 md:mb-0">CAREERS</li>
-//         </ul>
-//       </div>
-//       <div className="md:hidden">
-//         <img src="../img/menu icon.svg" alt="menu"/>
-//       </div>
-//       <div>
-//         hello
-//       </div>
-//     </nav>
-//   );
-// }
-
 import { useState } from "react";
 
 export default function DesktopNav() {
@@ -38,6 +14,8 @@ export default function DesktopNav() {
 
   return (
     <nav className="bg-blueishPurple p-5 text-white sticky top-0 flex justify-between items-center">
+
+      {/* normal desktop */}
       <span className="">
         <img src="../img/AT Digital Logo.svg" alt="logo" />
       </span>
@@ -72,6 +50,7 @@ export default function DesktopNav() {
           </div>
         </div>
       )}
+
     </nav>
   );
 }
