9082cf865afeb40ee2740a16d7417868198757cb
chamath-shanaka <chamathshanaka19@gmail.com>
2024-04-06 23:54:58 +0530

    img change at 1440px

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,7 +1,32 @@
+import { useState, useEffect } from 'react';
+
 export default function Banner() {
+  const [imageSrc, setImageSrc] = useState('../img/main.png');
+
+  useEffect(() => {
+
+    // handle image change at 1440px
+    function handleResize() {
+      if (window.innerWidth >= 1440) {
+        setImageSrc('../img/wide-main.png');
+      } else {
+        setImageSrc('../img/main.png');
+      }
+    };
+
+    // add event listener
+    window.addEventListener('resize', handleResize);
+    handleResize(); // call on mount
+
+    // when un-mounts
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
     <section className="relative">
-      <img src="../img/main.png" alt="main pic" />
+      <img src={imageSrc} alt="main pic" />
 
       <div 
         className="s1200px:absolute s1200px:left-20 s1200px:bottom-10
